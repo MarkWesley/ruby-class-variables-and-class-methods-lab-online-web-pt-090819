e10bc09224e0cf124b177bf709aedc1327ca1068
Mark Stone <markwstone85@gmail.com>
2019-09-29 20:14:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,8 @@ class Song
     @name = name
     @artist = artist
     @genre = genre
+    @@count +=1
+    @@genres << genre
   end
 
   attr_accessor :name
@@ -18,7 +20,7 @@ class Song
   end
 
   def self.genres
-    @@genres.uniq
+    @@genres.uniq!
   end
 
 end
