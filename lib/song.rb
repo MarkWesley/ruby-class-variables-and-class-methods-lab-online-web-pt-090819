f8152b57429b7b4bcc328b3dbfc6577f570914e8
Mark Stone <markwstone85@gmail.com>
2019-09-29 19:22:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,15 @@
 class Song
-  attr_accessor :name
-  attr_accessor :artist
-  attr_accessor :genre
 
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
   end
+
+  attr_accessor :name
+  attr_accessor :artist
+  attr_accessor :genre
+
+
+
 end
